91c4136d7c301a8901586b7e6979b3423c011d2e
Óscar Caro <oscaro@ucm.es>
2020-02-06 15:56:39 +0100

    Road methods created

--- src/simulator/model/Road.java
+++ src/simulator/model/Road.java
@@ -1,15 +1,21 @@
 package simulator.model;
 
+import java.util.List;
+
 import org.json.JSONObject;
 
-public class Road extends SimulatedObject {
+public  abstract class Road extends SimulatedObject {
 
 	private Junction srcJunc;
 	private Junction destJunc;
 	private int maxSpeed;
+	private int currSpeedLimit;
 	private int contLimit;
+	private int totalCont;
 	private int length;
 	private Weather weather;
+	private List<Vehicle> vehicles;
+	
 	Road(String id, Junction srcJunc, Junction destJunc, int maxSpeed, int contLimit, 
 			int length, Weather weather) { 
 		super(id); 
@@ -19,12 +25,10 @@ public class Road extends SimulatedObject {
 		this.contLimit = contLimit;
 		this.length = length;
 		this.weather = weather;
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
-	void advance(int time) {
-		// TODO Auto-generated method stub
+	public void advance(int time) {
 
 	}
 
@@ -33,13 +37,47 @@ public class Road extends SimulatedObject {
 		// TODO Auto-generated method stub
 		return null;
 	}
+	
+	void addContamination(int c) {
+		if(c >= 0) {
+			this.totalCont += c;
+		}
+		else {
+			throw new UnsupportedOperationException();
+		}
+	}
+	
+	abstract void reduceTotalContamination();
+	
+	abstract void updateSpeedLimit();
+	
+	abstract int calculateVehicleSpeed(Vehicle v);
+	
+	
+	
+	void enter(Vehicle v) {
+		//TODO
+	}
+	
+	void exit(Vehicle v) {
+		//TODO
+	}
 
 	public int getLength() {
 		return length;
 	}
 
-	public void setLength(int length) {
-		this.length = length;
+	public Junction getSrcJunc() {
+		return srcJunc;
 	}
 
+	public Junction getDestJunc() {
+		return destJunc;
+	}
+
+	void setWeather(Weather weather) {
+		this.weather = weather;
+	}
+	
+	
 }
