643dfa462cedc151c649a8096b183b5bc2309b32
Tvring <55629023+Tvring@users.noreply.github.com>
2020-02-16 18:36:01 +0100

    RoadMap constructor

--- src/simulator/model/RoadMap.java
+++ src/simulator/model/RoadMap.java
@@ -1,4 +1,25 @@
 package simulator.model;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class RoadMap {
+	private  List<Junction> juncList;
+	private  List<Road> roadList;
+	private  List<Vehicle> vehicleList;
+	private Map<String,Junction> juncMap;
+	private Map<String,Road> roadMap;
+	private Map<String,Vehicle> vehicleMap;
+	
+	public RoadMap() {
+		juncList = new ArrayList<Junction>();
+		roadList = new ArrayList<Road>();
+		vehicleList = new ArrayList<Vehicle>();
+		juncMap = new HashMap<String, Junction>();
+		roadMap = new HashMap<String, Road>();
+		vehicleMap = new HashMap<String, Vehicle>();
+		
+	}
 }
