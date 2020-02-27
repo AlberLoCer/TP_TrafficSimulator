adb28ad8aea5a1935d6a8f59166dcd066477f358
Tvring <55629023+Tvring@users.noreply.github.com>
2020-02-27 14:49:07 +0100

    Create NewVehicleEventBuilder.java

---
+++ src/simulator/factories/NewVehicleEventBuilder.java
@@ -0,0 +1,41 @@
+package simulator.factories;
+
+import java.util.ArrayList;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import simulator.model.Event;
+import simulator.model.NewVehicleEvent;
+
+public class NewVehicleEventBuilder extends Builder<Event> {
+
+	public static final String type = "new_vehicle";
+
+	public static final String timeKey = "time";
+	public static final String idKey = "id";
+	public static final String maxSpeedKey = "maxspeed";
+	public static final String contClassKey = "class";
+	public static final String itineraryKey = "itinerary";
+	
+	NewVehicleEventBuilder() {
+		super(type);
+	}
+
+	@Override
+	protected Event createTheInstance(JSONObject data) {
+		int time = data.getInt(timeKey);
+		String id = data.getString(idKey);
+		int maxSpeed = data.getInt(maxSpeedKey);
+		int contClass = data.getInt(contClassKey);
+		
+		JSONArray ja = data.getJSONArray(itineraryKey);
+		ArrayList<String> itineraryId = new ArrayList<String>();
+		for (int i = 0; i < ja.length(); i++) {
+			itineraryId.add(ja.getString(i));
+		}
+		
+		return new NewVehicleEvent(time, id, maxSpeed, contClass, itineraryId);
+	}
+
+}
