e5da246a79804ceeaab50f35763d48ff5b1050e7
Tvring <55629023+Tvring@users.noreply.github.com>
2020-02-27 15:22:29 +0100

    Create SetContClassEventBuilder.java

---
+++ src/simulator/factories/SetContClassEventBuilder.java
@@ -0,0 +1,45 @@
+package simulator.factories;
+
+import java.util.ArrayList;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import simulator.misc.Pair;
+import simulator.model.Event;
+import simulator.model.NewSetContClassEvent;
+public class SetContClassEventBuilder extends Builder<Event> {
+
+	public static final String type = "set_cont_class";
+	public static final String timeKey = "time";
+	public static final String infoKey = "info";
+	
+	public static final String vehicleSubKey = "vehicle";
+	public static final String contClassSubKey = "class";
+	
+	public SetContClassEventBuilder() {
+		super(type);		
+	}
+
+	@Override
+	protected Event createTheInstance(JSONObject data) {
+		int time = data.getInt(timeKey);
+		
+		JSONArray ja = data.getJSONArray(infoKey);
+		ArrayList<Pair<String, Integer>> pairList = new ArrayList<>();
+		ArrayList<JSONObject> jList = new ArrayList<>();
+
+		for (int i = 0; i < ja.length(); i++) {
+			jList.add(ja.getJSONObject(i));
+		}
+		
+		for (JSONObject currJson : jList) {
+			String vehicleId = currJson.getString(vehicleSubKey);
+			Integer contClass = Integer.parseInt(currJson.getString(contClassSubKey));
+			Pair<String, Integer> currPair = new Pair<String, Integer>(vehicleId, contClass);
+			pairList.add(currPair);
+		}
+		
+		return new NewSetContClassEvent(time, pairList);
+	}
+}
