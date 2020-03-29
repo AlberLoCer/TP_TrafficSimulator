e9aa4f592096518a638bcacc3c15b9f4183badd0
Óscar Caro <oscaro@ucm.es>
2020-03-29 16:40:33 +0200

    Events table model done

--- src/simulator/view/EventsTableModel.java
+++ src/simulator/view/EventsTableModel.java
@@ -14,20 +14,19 @@ public class EventsTableModel extends AbstractTableModel implements TrafficSimOb
 	private static final long serialVersionUID = 1L;
 	
 	private List<Event> events;
-	private String[] colNames = { "#", "Time", "Desc." };
+	private String[] colNames = { "Time", "Desc." };
 	
 
 	public EventsTableModel(Controller controller) {
 		controller.addObserver(this);
-		events=null;    
-		// TODO: controller.getSim().getEventlist()  to get the list of events 
-		// calling setEventsList( List<Event> events );
+		// This method will trigger a call to onRegister(), 
+		// in which we can get the initial list of events
 	}
 
 	public void update() {
-		// observar que si no refresco la tabla no se carga
-		// La tabla es la represantación visual de una estructura de datos,
-		// en este caso de un ArrayList, hay que notificar los cambios.
+		// This model holds the data.
+		// The table holds the visual view.
+		// So we need to notify the table that the data here has changed
 		
 		// We need to notify changes, otherwise the table does not refresh.
 		fireTableDataChanged();		
@@ -43,47 +42,33 @@ public class EventsTableModel extends AbstractTableModel implements TrafficSimOb
 		return false;
 	}
 
-	//si no pongo esto no coge el nombre de las columnas
-	//
 	//this is for the column header
 	@Override
 	public String getColumnName(int col) {
 		return colNames[col];
 	}
-
-	@Override
-	// método obligatorio, probad a quitarlo, no compila
-	//
+	
 	// this is for the number of columns
+	@Override
 	public int getColumnCount() {
 		return colNames.length;
 	}
-
-	@Override
-	// método obligatorio
-	//
+	
 	// the number of row, like those in the events list
+	@Override
 	public int getRowCount() {
 		return events == null ? 0 : events.size();
 	}
 
+	// returns the value of a particular cell (to display it visually)
 	@Override
-	// metodo obligatorio
-	// asi es como se va a cargar la tabla desde el ArrayList
-	// el indice del arrayList es el numero de fila porque en este ejemplo
-	// quiero enumerarlos.
-	//
-	// returns the value of a particular cell 
 	public Object getValueAt(int rowIndex, int columnIndex) {
 		Object s = null;
 		switch (columnIndex) {
 		case 0:
-			s = rowIndex;
-			break;
-		case 1:
 			s = events.get(rowIndex).getTime();
 			break;
-		case 2:
+		case 1:
 			s = events.get(rowIndex).toString();
 			break;
 		}
@@ -92,37 +77,31 @@ public class EventsTableModel extends AbstractTableModel implements TrafficSimOb
 
 	@Override
 	public void onAdvanceStart(RoadMap map, List<Event> events, int time) {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void onAdvanceEnd(RoadMap map, List<Event> events, int time) {
-		// TODO Auto-generated method stub
-		
+		setEventsList(events);			
 	}
 
 	@Override
 	public void onEventAdded(RoadMap map, List<Event> events, Event e, int time) {
-		// TODO Auto-generated method stub
-		
+		setEventsList(events);			
 	}
 
 	@Override
 	public void onReset(RoadMap map, List<Event> events, int time) {
-		// TODO Auto-generated method stub
-		
+		setEventsList(events);	
 	}
 
 	@Override
 	public void onRegister(RoadMap map, List<Event> events, int time) {
-		// TODO Auto-generated method stub
-		
+		setEventsList(events);		
 	}
 
 	@Override
-	public void onError(String err) {
-		// TODO Auto-generated method stub
+	public void onError(String err) {		
 		
 	}
 }
