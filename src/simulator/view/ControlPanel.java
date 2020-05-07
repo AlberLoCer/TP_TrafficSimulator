4e0e397eb33689a5d1f4f67b3d84456944f59916
Óscar Caro <oscaro@ucm.es>
2020-05-07 15:10:35 +0200

    Remove unused import

--- src/simulator/view/ControlPanel.java
+++ src/simulator/view/ControlPanel.java
@@ -4,7 +4,6 @@ import java.awt.Dimension;
 import java.awt.Frame;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.util.List;
 
